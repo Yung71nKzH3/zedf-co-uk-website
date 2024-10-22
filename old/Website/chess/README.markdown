918c8606864ff42cae986049387d62048d99092b
w1ll0w <58903678+Yung71nKzH3@users.noreply.github.com>
2024-10-22 15:11:57 +0100

    Initial Upload 1

---
+++ old/Website/chess/README.markdown
@@ -0,0 +1,6 @@
+# Chess
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/viethoang012/pen/xRNgyM](https://codepen.io/viethoang012/pen/xRNgyM).
+
+Chess for 2 players
++ Responsive design
---
+++ old/javascript.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var horses = document.getElementsByClassName("horses");
+  var dots = document.getElementsByClassName("dot");
+  if (n > horses.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = horses.length}
+    for (i = 0; i < horses.length; i++) {
+      horses[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  horses[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
